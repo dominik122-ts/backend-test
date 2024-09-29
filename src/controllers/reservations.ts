165588e7c4a3457a3e0865714a2f19b1a8f6aceb
dominik122-ts <dominik.webdev@gmail.com>
2024-09-29 05:01:48 +0000

    feat: add pagination in reservations query

--- src/controllers/reservations.ts
+++ src/controllers/reservations.ts
@@ -1,7 +1,31 @@
 import asyncHandler from "express-async-handler";
+import type { PaginationQuery } from "../types";
 import { prisma } from "..";
 
-export const getReservations = asyncHandler(async (req, res) => {
-  const reservations = await prisma.reservation.findMany();
-  res.json(reservations);
-});
+export const getReservations = asyncHandler<{}, {}, {}, PaginationQuery>(
+  async (req, res) => {
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+
+    const [reservations, totalReservations] = await Promise.all([
+      prisma.reservation.findMany({
+        skip: page ? (page - 1) * limit : 0,
+        take: limit,
+        include: { User: true, Table: true }
+      }),
+      prisma.reservation.count()
+    ]);
+
+    const totalPages = Math.ceil(totalReservations / limit);
+
+    res.json({
+      reservations,
+      meta: {
+        totalReservations,
+        totalPages,
+        currentPage: page,
+        pageSize: limit
+      }
+    });
+  }
+);
--- src/types.d.ts
+++ src/types.d.ts
@@ -4,3 +4,8 @@ export interface CreateUserRequestBody {
   reservationTime: string;
   reservationTable: number;
 }
+
+export interface PaginationQuery {
+  page?: number;
+  limit?: number;
+}
